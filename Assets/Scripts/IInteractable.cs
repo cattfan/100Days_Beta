2f65f61d994f95e8f8bca1a203fe95478ed3b9ed
ChiHaiNguyen <Haiforwork2005@gmail.com>
2025-08-16 16:05:53 +0700

    Sua loi UI shop lan 1 va bo sung script interactable

---
+++ Assets/Scripts/IInteractable.cs
@@ -0,0 +1,8 @@
+﻿public interface IInteractable
+{
+    // Method to handle interaction with the object
+    void Interact();
+    // Method to check if the object can be interacted with
+    bool CanInteract();
+    // Optional: Method to get the interaction message or prompt
+}
